ff6b2a2bc1d43cde5e4410abac681e068c78369a
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-09-26 16:44:53 +0500

    About section completed

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,29 +1,13 @@
 import { Card } from "@/components/Card";
 import Image from "next/image";
 import Book from "@/public/assets/icons/book.jpg";
-import ReactIcon from "@/public/assets/icons/React.png";
-import Mongo from "@/public/assets/icons/MongoDB.png";
-import Node from "@/public/assets/icons/Node.png";
-import TypeScript from "@/public/assets/icons/TypeScript.png";
-import Tailwind from "@/public/assets/icons/Tailwind.png";
-import Postgres from "@/public/assets/icons/Postgres.png";
 import StarIcon from "@/public/assets/icons/star.svg";
-import { TechIcon } from "@/components/TechIcon";
-
-const tools = [
-  { icon: ReactIcon, title: "React" },
-  { icon: Mongo, title: "MongoDB" },
-  { icon: Node, title: "Node" },
-  { icon: Tailwind, title: "Tailwind" },
-  { icon: TypeScript, title: "TypeScript" },
-  { icon: Postgres, title: "PostgresSQL" },
-];
-
+import Emoji from "@/public/assets/images/Me.png";
 export const AboutSection = () => {
   return (
-    <section>
+    <section className="py-20 lg:py-22">
       <div className="container">
-        <p className="uppercase font-semibold tracking-widest text-neutral-300 text-center">
+        <p className="uppercase font-semibold tracking-widest text-secondary text-center">
           About Me
         </p>
         <h2 className="font-sans font-semibold text-3xl md:text-5xl mt-6 text-center">
@@ -33,46 +17,38 @@ export const AboutSection = () => {
           See my day-to-day life, my hobbies, and what I do when I&apos;m not
           working.
         </p>
-        <div className="mt-8 gap-8">
-          <Card className="h-[320px]">
-            <div className="flex flex-col">
-              <div className="inline-flex items-center gap-2">
-                <StarIcon className="size-9 text-secondary" />
-                <h3 className="text-3xl">Current Reads</h3>
-              </div>
-              <p className=" text-sm mt-2 text-center">
-                The books I&apos;m currently reading.
-              </p>
-            </div>
-            <div className="w-40 mx-auto mt-6">
-              <Image src={Book} alt="Cover of book" />
-            </div>
-          </Card>
-          <Card className="pb-4">
-            <div className="flex flex-col">
-              <div className="inline-flex items-center gap-2">
-                <StarIcon className="size-9 text-secondary" />
-                <h3 className="text-3xl">My Tech Stack</h3>
-              </div>
-              <p className=" text-sm mt-2 text-ceter">
-                The tools I use to make exceptional digital experiences
-              </p>
-            </div>
-            <div className="flex flex-wrap mx-auto gap-4 mt-4">
-              {tools.map((item) => (
-                <div
-                  key={item.title}
-                  className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-secondary"
-                >
-                  <span>{item.title}</span>
+        <div className="flex flex-col mt-10 md:mt-20 gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 md:gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <div className="flex flex-col">
+                <div className="inline-flex items-center gap-2">
+                  <StarIcon className="size-9 text-secondary" />
+                  <h3 className="text-3xl">Books</h3>
                 </div>
-              ))}
-            </div>
-          </Card>
-          <Card>
-            <h3>Intrests and Hobbies</h3>
-            <p>Explore what other things I&apos;m passionate about.</p>
-          </Card>
+                <p className=" text-sm mt-2 text-center">
+                  The books I&apos;m currently reading.
+                </p>
+              </div>
+              <div className="w-40 mx-auto mt-6">
+                <Image src={Book} alt="Cover of book" />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <div className="flex flex-col">
+                <div className="inline-flex items-center gap-2">
+                  <StarIcon className="size-9 text-secondary" />
+                  <h3 className="text-3xl sm:text-2xl">Current Projects</h3>
+                </div>
+                <p className=" text-sm mt-2">
+                  Currently pursuing my bachelor&apos;s degree in Computer
+                  Science and working on my final year project.
+                </p>
+              </div>
+              <div className="w-40 mx-auto mt-6">
+                <Image src={Emoji} alt="" />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
